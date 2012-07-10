92bc98a17945514df8a5c7de944e78688fcdab2b
Neil Alexander <neilalexander@frozenriver.net>
2012-07-10 19:04:16 +0100

    Updating build script

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ rm -f underlink
 gcc -g -c	main.c		-o main.o
 gcc -g -c	bucket.c 	-o bucket.o
 gcc -g -c	node.c		-o node.o
-gcc -g		main.o bucket.o	node.o \
+gcc -g -c 	message.c	-o message.o
+gcc -g		main.o bucket.o	node.o message.o \
 				-o underlink
 
 ./underlink
