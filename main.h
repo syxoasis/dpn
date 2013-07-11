640899a749811c422d1a5d67a56de7ebf3b141a2
Neil Alexander <neilalexander@frozenriver.net>
2013-07-11 21:19:47 +0100

    Added building nacl crypto, early crypto code

--- main.h
+++ main.h
@@ -1,11 +1,12 @@
 // The number of bits that makes up a single address
-#define ADDR_LEN 48
+#define ADDR_LEN 64
 
 // The maximum number of nodes in each bucket
 #define NODES_PER_BUCKET 24
 
 // Maximum transmission unit supported
 #define MTU 1500
+#define MAX_BUFFER_SIZE 1546
 
 // Print debug output to stdout
 #define debug 1
