29517056d4b8e8f61edffc02152f18404434d227
Neil Alexander <neilalexander@frozenriver.net>
2013-07-13 00:47:43 +0100

    Revert "Added building nacl crypto, early crypto code"
    This reverts commit 640899a749811c422d1a5d67a56de7ebf3b141a2.

--- main.h
+++ main.h
@@ -1,12 +1,11 @@
 // The number of bits that makes up a single address
-#define ADDR_LEN 64
+#define ADDR_LEN 48
 
 // The maximum number of nodes in each bucket
 #define NODES_PER_BUCKET 24
 
 // Maximum transmission unit supported
 #define MTU 1500
-#define MAX_BUFFER_SIZE 1546
 
 // Print debug output to stdout
 #define debug 1
