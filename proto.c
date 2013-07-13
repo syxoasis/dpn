5326793ece00374aaace17d6a60cf24152467a80
Neil Alexander <neilalexander@frozenriver.net>
2013-07-13 19:31:22 +0100

    Revert "Revert "Added building nacl crypto, early crypto code""
    This reverts commit 29517056d4b8e8f61edffc02152f18404434d227.

---
+++ proto.c
@@ -0,0 +1,210 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include "main.h"
+#include "include/crypto_box_curve25519xsalsa20poly1305.h"
+#include "include/crypto_scalarmult_curve25519.h"
+
+#define uint64 unsigned long long
+#define noncelength 16
+#define nonceoffset (crypto_box_curve25519xsalsa20poly1305_NONCEBYTES - noncelength)
+
+struct tai
+{
+	uint64 x;
+};
+
+struct taia
+{
+	struct tai sec;
+	unsigned long nano;
+	unsigned long atto;
+};
+
+typedef struct proto_nacl
+{
+	unsigned char privatekey[crypto_box_curve25519xsalsa20poly1305_SECRETKEYBYTES];
+	unsigned char publickey[crypto_box_curve25519xsalsa20poly1305_PUBLICKEYBYTES];
+	unsigned char precomp[crypto_box_curve25519xsalsa20poly1305_BEFORENMBYTES];
+	unsigned char encnonce[crypto_box_curve25519xsalsa20poly1305_NONCEBYTES];
+	unsigned char decnonce[crypto_box_curve25519xsalsa20poly1305_NONCEBYTES];
+	
+	struct taia cdtaip, cdtaie;
+}
+proto_nacl;
+
+void tai_pack(char *s, struct tai *t)
+{
+	uint64 x;
+	x = t->x;
+	s[7] = x & 255; x >>= 8;
+	s[6] = x & 255; x >>= 8;
+	s[5] = x & 255; x >>= 8;
+	s[4] = x & 255; x >>= 8;
+	s[3] = x & 255; x >>= 8;
+	s[2] = x & 255; x >>= 8;
+	s[1] = x & 255; x >>= 8;
+	s[0] = x;
+}
+
+void tai_unpack(char *s, struct tai *t)
+{
+	uint64 x;
+	x = (unsigned char) s[0];
+	x <<= 8; x += (unsigned char) s[1];
+	x <<= 8; x += (unsigned char) s[2];
+	x <<= 8; x += (unsigned char) s[3];
+	x <<= 8; x += (unsigned char) s[4];
+	x <<= 8; x += (unsigned char) s[5];
+	x <<= 8; x += (unsigned char) s[6];
+	x <<= 8; x += (unsigned char) s[7];
+	t->x = x;
+}
+
+void taia_pack(char *s, struct taia *t)
+{
+	unsigned long x;
+	tai_pack(s, &t->sec);
+	s += 8;
+	x = t->atto;
+	s[7] = x & 255; x >>= 8;
+	s[6] = x & 255; x >>= 8;
+	s[5] = x & 255; x >>= 8;
+	s[4] = x;
+	x = t->nano;
+	s[3] = x & 255; x >>= 8;
+	s[2] = x & 255; x >>= 8;
+	s[1] = x & 255; x >>= 8;
+	s[0] = x;
+} 
+
+void taia_unpack(char *s, struct taia *t)
+{
+	unsigned long x;
+	tai_unpack(s, &t->sec);
+	s += 8;
+	x = (unsigned char) s[4];
+	x <<= 8; x += (unsigned char) s[5];
+	x <<= 8; x += (unsigned char) s[6];
+	x <<= 8; x += (unsigned char) s[7];
+	t->atto = x;
+	x = (unsigned char) s[0];
+	x <<= 8; x += (unsigned char) s[1];
+	x <<= 8; x += (unsigned char) s[2];
+	x <<= 8; x += (unsigned char) s[3];
+	t->nano = x;
+}
+
+void taia_now(struct taia *t)
+{
+	struct timeval now;
+	gettimeofday(&now, (struct timezone *) 0);
+	t->sec.x = 4611686018427387914ULL + (uint64) now.tv_sec;
+	t->nano = 1000 * now.tv_usec + 500;
+	t->atto++;
+}
+
+static int proto_encode(proto_nacl *inst, unsigned char* input, unsigned char* output, unsigned int len)
+{
+	if (len + noncelength + crypto_box_curve25519xsalsa20poly1305_BOXZEROBYTES > MAX_BUFFER_SIZE)
+	{
+		fprintf(stderr, "Encryption failed (packet length %i is above MAX_BUFFER_SIZE %i)\n", len, MAX_BUFFER_SIZE);
+		return -1;
+	}
+		
+	unsigned char tempbufferinput[len + crypto_box_curve25519xsalsa20poly1305_ZEROBYTES];
+	
+	memset(tempbufferinput, 0, crypto_box_curve25519xsalsa20poly1305_ZEROBYTES);
+	memcpy(tempbufferinput + crypto_box_curve25519xsalsa20poly1305_ZEROBYTES, input, len);
+	
+	len += crypto_box_curve25519xsalsa20poly1305_ZEROBYTES;
+	
+	taia_now(&inst->cdtaie);
+	taia_pack(inst->encnonce + nonceoffset, &(inst->cdtaie));
+
+	int result = crypto_box_curve25519xsalsa20poly1305_afternm(
+		output,
+		tempbufferinput,
+		len,
+		inst->encnonce,
+		inst->precomp
+	);
+	
+	memcpy(output, inst->encnonce + nonceoffset, noncelength);
+	
+	if (result)
+	{
+		fprintf(stderr, "Encryption failed (length %i, given result %i)\n", len, result);
+		return -1;
+	}
+	
+	return len;
+}
+
+static int proto_decode(proto_nacl *inst, unsigned char* input, unsigned char* output, unsigned int len)
+{
+	if (len - crypto_box_curve25519xsalsa20poly1305_ZEROBYTES > MAX_BUFFER_SIZE)
+	{
+		fprintf(stderr, "Decryption failed (packet length %i is above MAX_BUFFER_SIZE %i)\n", len, MAX_BUFFER_SIZE);
+		return 0;
+	}
+	
+	if (len < crypto_box_curve25519xsalsa20poly1305_BOXZEROBYTES + noncelength)
+	{
+		fprintf(stderr, "Short packet received: %d\n", len);
+		return 0;
+	}
+
+	struct taia cdtaic;
+	unsigned char tempbufferout[len];
+	
+	taia_unpack((char*) input, &cdtaic);
+
+	memcpy(inst->decnonce + nonceoffset, input, noncelength);
+	memset(input, 0, crypto_box_curve25519xsalsa20poly1305_BOXZEROBYTES);
+	
+	int result = crypto_box_curve25519xsalsa20poly1305_open_afternm(
+		tempbufferout,
+		input,
+		len,
+		inst->decnonce,
+		inst->precomp
+	);
+	
+	inst->cdtaip = cdtaic;
+	
+	if (result)
+	{
+		fprintf(stderr, "Decryption failed (length %i, given result %i)\n", len, result);
+		return 0;
+	}
+	
+	len -= crypto_box_curve25519xsalsa20poly1305_ZEROBYTES;
+	memcpy(output, tempbufferout + crypto_box_curve25519xsalsa20poly1305_ZEROBYTES, len);
+	
+	return len;
+}
+
+static int proto_init(proto_nacl *instance)
+{
+	proto_nacl* inst = ((proto_nacl*) instance);
+	unsigned char taipublickey[crypto_box_curve25519xsalsa20poly1305_PUBLICKEYBYTES];
+	
+	crypto_box_curve25519xsalsa20poly1305_beforenm(
+		inst->precomp,
+		inst->publickey,
+		inst->privatekey
+	);
+	
+	memset(inst->encnonce, 0, crypto_box_curve25519xsalsa20poly1305_NONCEBYTES);
+	memset(inst->decnonce, 0, crypto_box_curve25519xsalsa20poly1305_NONCEBYTES);
+	
+	crypto_scalarmult_curve25519_base(taipublickey, inst->privatekey);
+	
+	inst->encnonce[nonceoffset - 1] = memcmp(taipublickey, inst->publickey, crypto_box_curve25519xsalsa20poly1305_PUBLICKEYBYTES) > 0 ? 1 : 0;
+	inst->decnonce[nonceoffset - 1] = inst->encnonce[nonceoffset - 1] ? 0 : 1;
+	
+	return 0;
+}
