b16baffb3f021eeb5d546f77ee5f9f72c35223ba
Neil Alexander <neilalexander@frozenriver.net>
2013-07-23 00:31:49 +0100

    Reverting previous change to destination check

--- main.c
+++ main.c
@@ -298,7 +298,7 @@ int main(int argc, char* argv[])
 			switch (message.message)
 			{
 				case IPPACKET:
-					if (uint128_equals(message.remoteID, thisNode.nodeID))
+					if (memcmp(&src_addr->s6_addr, &thisNode.nodeID, sizeof(underlink_nodeID)) != 0)
 					{
 						if (write(tuntapfd, message.packetbuffer, message.payloadsize) <= 0)
 						{
