4cec0cebc055ded24a2cb99c74999f8c6ec9891f
Neil Alexander <neilalexander@frozenriver.net>
2013-07-24 19:51:56 +0100

    Fixed routing to TUN

--- main.c
+++ main.c
@@ -299,7 +299,7 @@ int main(int argc, char* argv[])
 			switch (message.message)
 			{
 				case IPPACKET:
-					if (memcmp(src_addr, &thisNode.nodeID, sizeof(underlink_nodeID)) != 0)
+					if (memcmp(&dst_addr, &thisNode.nodeID, sizeof(underlink_nodeID)) != 0)
 					{
 						if (write(tuntapfd, message.packetbuffer, message.payloadsize) <= 0)
 						{
